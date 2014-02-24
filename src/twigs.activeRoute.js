001a2eeb0e3b92f2406c7fc5d88eac1dc19e588d
xeronimus <xeronimus@gmail.com>
2014-02-24 12:13:44 +0100

    moved sources from app to src

--- src/twigs.activeRoute.js
+++ src/twigs.activeRoute.js
@@ -1,26 +1,13 @@
-/* twigs
- * Copyright (C) 2014, Hatch Development Team
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 'use strict';
 
 /**
- *  Marks an element as 'active', if the specified regex matches the current route
+ * @ngdoc directive
+ * @name twigs.activeRoute:twgActiveRoute
+ * @element ANY
  *
- *  See readme.md for more information
+ * @description
+ * Marks an element as 'active', if the specified regex matches the current route
+ *
  */
 angular.module('twigs.activeRoute', [])
 
