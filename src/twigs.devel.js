684fc4437c6215d50f441358bdc4a67b1e96cd96
xeronimus <xeronimus@gmail.com>
2014-02-24 15:57:30 +0100

    #9 developmentInfoFooter

---
+++ src/twigs.devel.js
@@ -0,0 +1,220 @@
+'use strict';
+
+/* twigs
+ * Copyright (C) 2014, Hatch Development Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+angular.module('twigs.devel', [ ])
+
+    .constant('ERROR_REPORTED_EVENT', 'twigs.devel.errorReported')
+    .constant('SERVER_REQUEST_REPORTED_EVENT', 'twigs.devel.serverRequestReported')
+
+
+/**
+ * @ngdoc object
+ * @name twigs.devel.service:DevelopmentInfoServiceProvider
+ *
+ * @description
+ * Inject into your config function to set a url filter pattern.
+ *
+ * ```javascript
+ * App.config(function (DevelopmentInfoServiceProvider) {
+ *   DevelopmentInfoServiceProvider.setUrlFilterPattern(/\/ws\/rest\//);
+ * });
+ * ```
+ */
+    .provider('DevelopmentInfoService', function () {
+        var urlFilterPattern = /.*/;
+
+        /**
+         * @ngdoc function
+         * @name  twigs.devel.service:DevelopmentInfoServiceProvider#setUrlFilterPattern
+         * @methodOf twigs.devel.service:DevelopmentInfoServiceProvider
+         *
+         * @description
+         * Pass in a regex pattern (e.g. `/\/ws\/rest\//`) to filter XHTTP request urls. Most of the time,
+         * you only want to include REST request and exclude angular's request to templates etc.
+         *
+         * @param {string} pattern A regex pattern to filter XHTTP Request urls
+         */
+        this.setUrlFilterPattern = function (pattern) {
+            urlFilterPattern = pattern;
+        };
+
+        /**
+         * @ngdoc service
+         * @name twigs.devel.service:DevelopmentInfoService
+         *
+         * @description
+         * Provides functionality for gathering and displaying devel/debug information.
+         *
+         * Use in your services to report errors.
+         *
+         * ```javascript
+         *   function(DevelopmentInfoService){
+         *       DevelopmentInfoService.reportError('some name', {some:'payload data'});
+         *   }
+         * ```
+         */
+        this.$get = function ($rootScope, ERROR_REPORTED_EVENT, SERVER_REQUEST_REPORTED_EVENT) {
+            var errors = [] , serverRequests = [];
+
+            function reportError(name, data) {
+                errors.push({
+                    name: name,
+                    payload: data,
+                    date: new Date()
+                });
+
+                $rootScope.$broadcast(ERROR_REPORTED_EVENT, errors);
+            }
+
+            function reportServerRequest(url, response, status) {
+                serverRequests.push({
+                    url: url,
+                    response: response,
+                    status: status,
+                    showResponse: false,
+                    date: new Date()
+                });
+                $rootScope.$broadcast(SERVER_REQUEST_REPORTED_EVENT, serverRequests);
+            }
+
+            function getUrlFilterPattern() {
+                return urlFilterPattern;
+            }
+
+            var developmentInfoService = {
+
+                /**
+                 * @ngdoc function
+                 * @name  twigs.devel.service:DevelopmentInfoService#reportError
+                 * @methodOf twigs.devel.service:DevelopmentInfoService
+                 *
+                 * @description
+                 * Invoke this to report an error that happend in your application
+                 *
+                 * @param {string} name The name of the error
+                 * @param {object} data Error payload that will be displayed
+                 */
+                reportError: reportError,
+
+                /**
+                 *  Only invoked by our response interceptor. No need to use that from outside.
+                 */
+                reportServerRequest: reportServerRequest,
+
+                getUrlFilterPattern: getUrlFilterPattern
+            };
+
+            return developmentInfoService;
+        };
+    })
+
+/**
+ * @ngdoc object
+ * @name twigs.devel.controller:DevelopmentInfoCtrl
+ *
+ * @description
+ * Provides a scope which contains the gathered development Information.
+ *
+ * Use this controller in your markup to get access to the gathered Information.
+ *
+ *
+ * Example:
+ * ```html
+ * <footer id="devel-footer" ng-controller="DevelopmentInfoCtrl">
+ *     <div class="container">
+ *         <div class="page-header">
+ *             <h3>Development Info</h3>
+ *         </div>
+ *
+ *         <h4>Errors</h4>
+ *
+ *         <div ng-repeat="error in errors" class="devel-error">
+ *             <h4>
+ *                 <span class="label label-danger">{{error.name}}</span>
+ *                 <small>{{error.date}}</small>
+ *             </h4>
+ *             {{error.payload | json}}
+ *         </div>
+ *
+ *         <h4>REST Requests</h4>
+ *
+ *         <div ng-repeat="request in serverRequests" class="devel-request">
+ *             <h4><span class="label " ng-class="{'label-success':request.status === 200,'label-danger':request.status !== 200}">{{request.status}}</span>
+ *                 <button class="btn btn-xs btn-default" ng-click="request.showResponse = !request.showResponse"
+ *                         translate>toggle_response
+ *                 </button>
+ *                 <small>{{request.url}}</small>
+ *             </h4>
+ *             <div ng-show="request.showResponse">{{request.response | json}}</div>
+ *         </div>
+ *     </div>
+ * </footer>
+ * ```
+ */
+    .controller('DevelopmentInfoCtrl', function ($rootScope, $scope, $permission, $location, ERROR_REPORTED_EVENT, SERVER_REQUEST_REPORTED_EVENT) {
+
+        $scope.develFooterEnabled = ($location.search().develFooter === 'true');
+
+        $scope.$on(ERROR_REPORTED_EVENT, function (event, allErrors) {
+            $scope.errors = allErrors;
+        });
+        $scope.$on(SERVER_REQUEST_REPORTED_EVENT, function (event, allRequests) {
+            $scope.serverRequests = allRequests;
+        });
+    })
+
+
+/**
+ * Registers a responseInterceptor that reports all xhttpResponses that
+ * devel footer will then display this information
+ */
+    .config(['$httpProvider', function ($httpProvider) {
+
+        function interceptor($q, DevelopmentInfoService) {
+
+            var urlFilterPattern = DevelopmentInfoService.getUrlFilterPattern();
+
+            function isMatchingFilterPattern(url) {
+                return urlFilterPattern.test(url);
+            }
+
+            // default behaviour
+            function success(response) {
+                if (isMatchingFilterPattern(response.config.url)) {
+                    DevelopmentInfoService.reportServerRequest(response.config.url, response.data, response.status);
+                }
+                return response;
+            }
+
+            function error(response) {
+                if (isMatchingFilterPattern(response.config.url)) {
+                    DevelopmentInfoService.reportServerRequest(response.config.url, response.data, response.status);
+                }
+                // default behaviour
+                return $q.reject(response);
+            }
+
+            // default behaviour
+            return function (promise) {
+                return promise.then(success, error);
+            };
+        }
+
+        $httpProvider.responseInterceptors.push(interceptor);
+    }]);
---
+++ test/spec/twigs.devel_spec.js
@@ -0,0 +1,132 @@
+/* twigs
+ * Copyright (C) 2014, Hatch Development Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+'use strict';
+
+describe('Service Devel', function () {
+
+    var dummyErrorName = 'Something went terribly wrong';
+    var dummyErrorPayload = {
+        'what': 'I lost my twigs'
+    };
+    var dummyUrl = 'http://some/url';
+    var dummyResponse = {some: 'data response'};
+    var dummyStatus = 200;
+
+
+    var DevelopmentInfoServiceProvider;
+
+    beforeEach(function () {
+        // Initialize the service provider by injecting it to a fake module's config block
+        var fakeModule = angular.module('testApp', function () {
+        });
+
+        fakeModule.config(function (_DevelopmentInfoServiceProvider_) {
+            DevelopmentInfoServiceProvider = _DevelopmentInfoServiceProvider_;
+        });
+        // Initialize ht.flow module injector
+        angular.mock.module('twigs.devel', 'testApp');
+
+        // Kickstart the injectors previously registered with calls to angular.mock.module
+        inject(function () {
+        });
+    });
+
+    // instantiate service
+    var DevelopmentInfoService, $rootScope, scope, ERROR_REPORTED_EVENT, SERVER_REQUEST_REPORTED_EVENT;
+    beforeEach(inject(function (_DevelopmentInfoService_, _$rootScope_, _ERROR_REPORTED_EVENT_, _SERVER_REQUEST_REPORTED_EVENT_) {
+        DevelopmentInfoService = _DevelopmentInfoService_;
+        $rootScope = _$rootScope_;
+        ERROR_REPORTED_EVENT = _ERROR_REPORTED_EVENT_;
+        SERVER_REQUEST_REPORTED_EVENT = _SERVER_REQUEST_REPORTED_EVENT_
+        scope = $rootScope.$new();
+    }));
+
+    describe('Provider', function () {
+
+        it('allows to set a urlFilter', function () {
+            var pattern = /\/ws\/rest\//;
+            DevelopmentInfoServiceProvider.setUrlFilterPattern(pattern);
+            expect(DevelopmentInfoService.getUrlFilterPattern()).toEqual(pattern);
+        });
+
+    });
+
+    describe('Service', function () {
+
+        it('allows to report an error', function () {
+            DevelopmentInfoService.reportError(dummyErrorName, dummyErrorPayload);
+        });
+
+        it('event is broadcasted on error report', function () {
+
+            var eventReceived = false;
+
+            /**
+             * attach dummy listener to error event
+             */
+            $rootScope.$on(ERROR_REPORTED_EVENT, function () {
+                eventReceived = true;
+            });
+
+            /*
+             * report an error
+             */
+            DevelopmentInfoService.reportError(dummyErrorName, dummyErrorPayload);
+
+            /*
+             * wait for event to be broadcasted
+             */
+            waitsFor(function () {
+                return eventReceived;
+            }, 'event received', 1000);
+
+        });
+
+        it('allows to report a server request', function () {
+            DevelopmentInfoService.reportServerRequest(dummyUrl, dummyResponse, dummyStatus);
+        });
+
+        it('event is broadcasted on request report', function () {
+
+            var eventReceived = false;
+
+            /**
+             * attach dummy listener to error event
+             */
+            $rootScope.$on(SERVER_REQUEST_REPORTED_EVENT, function () {
+                eventReceived = true;
+            });
+
+            /*
+             * report a server request
+             */
+            DevelopmentInfoService.reportServerRequest(dummyUrl, dummyResponse, dummyStatus);
+
+            /*
+             * wait for event to be broadcasted
+             */
+            waitsFor(function () {
+                return eventReceived;
+            }, 'event received', 1000);
+
+        });
+
+    });
+
+
+});
