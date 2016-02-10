04235202ca38b1fc2c3d61301257f1bb3f41e9b8
Sergio Trentini <xeronimus@gmail.com>
2016-02-10 16:19:03 +0100

    updated docu

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,215 @@
+NG_DOCS={
+  "sections": {
+    "api": "Twigs Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "twigs",
+      "shortName": "twigs",
+      "type": "overview",
+      "moduleName": "twigs",
+      "shortDescription": "The main module which collects all other Twigs modules.",
+      "keywords": "angular api app collects convenience dependency include javascript main module modules overview twigs var"
+    },
+    {
+      "section": "api",
+      "id": "twigs.activeRoute.directive:twgActiveRoute",
+      "shortName": "twgActiveRoute",
+      "type": "directive",
+      "moduleName": "twigs.activeRoute",
+      "shortDescription": "In almost every webpage you&#39;d like to mark elements as active, if the current view matches their link. E.g., in a navigation Menu,",
+      "keywords": "active active-route activeroute add api changes class complex css current currently directive element elements example flag highlighted href html item link listen mark match matches menu navigation ng-class regex route scope set true twg-active-route twgactive twigs url view webpage"
+    },
+    {
+      "section": "api",
+      "id": "twigs.devel.controller:DevelopmentInfoCtrl",
+      "shortName": "DevelopmentInfoCtrl",
+      "type": "object",
+      "moduleName": "twigs.devel",
+      "shortDescription": "Provides a scope which contains the gathered development Information.",
+      "keywords": "access api btn btn-default btn-xs class container controller devel devel-error devel-footer devel-request development developmentinfoctrl error errors example gathered html info json label label-danger label-success markup ng-class ng-click ng-controller ng-repeat ng-show object page-header payload request requests response scope serverrequests showresponse status translate twigs url"
+    },
+    {
+      "section": "api",
+      "id": "twigs.devel.service:DevelopmentInfoService",
+      "shortName": "DevelopmentInfoService",
+      "type": "service",
+      "moduleName": "twigs.devel",
+      "shortDescription": "Provides functionality for gathering and displaying devel/debug information.",
+      "keywords": "addition api application custom data devel developmentinfoservice display displayed displaying error errors footer function functionality gathering happend invoke javascript object payload register report reporterror service services twigs watch watchcustomdata"
+    },
+    {
+      "section": "api",
+      "id": "twigs.devel.service:DevelopmentInfoServiceProvider",
+      "shortName": "DevelopmentInfoServiceProvider",
+      "type": "object",
+      "moduleName": "twigs.devel",
+      "shortDescription": "Inject into your config function to set a url filter pattern.",
+      "keywords": "angular api app config devel developmentinfoserviceprovider exclude filter function include inject javascript object pass pattern regex request rest service set seturlfilterpattern templates time twigs url urls xhttp"
+    },
+    {
+      "section": "api",
+      "id": "twigs.flow.provider:FlowProvider",
+      "shortName": "FlowProvider",
+      "type": "object",
+      "moduleName": "twigs.flow",
+      "shortDescription": "In more complex applications we&#39;d like to chain different views into business processes, i.e. flows.",
+      "keywords": "$routeprovider access adds allowed allows angular api app application applications business chain completes complex config configuration controller controllers createflow creation current define definition definitions directly easy example flow flowid flowprovider flows function guide html invoke javascript jump main match matches mechanism model module mywizard object path previous proceed process processes properties provider route routes second separate service share shared simple skip starting starts step stepconfig steps string switch templateurl third transitions twigs unique user usual valid var views wizard-like wizardctrl"
+    },
+    {
+      "section": "api",
+      "id": "twigs.flow.service:Flow",
+      "shortName": "Flow",
+      "type": "object",
+      "moduleName": "twigs.flow",
+      "shortDescription": "Flow navigation",
+      "keywords": "$scope access allowed angular api check checks config controller current directly false flow flowmodel flows function getmodel good holds iscurrentstep javascript jump jumps main matches model module navigate navigation object onbuttonnext onbuttonprevious performs previous proceeds provider returns service set share shared step stepid steps targetstepid tostep transition true twigs wizardctrl"
+    },
+    {
+      "section": "api",
+      "id": "twigs.globalHotkeys.directive:twgGlobalHotkeys",
+      "shortName": "twgGlobalHotkeys",
+      "type": "directive",
+      "moduleName": "twigs.globalHotkeys",
+      "shortDescription": "This directive registers for the &#39;keydown&#39; event and forwards them to",
+      "keywords": "_html_ api buttons catch current custom defined directive element elements event events form forwards function global globalhotkeys hotkey hotkeys ignores key keydown keystrokes links order registers service twigs"
+    },
+    {
+      "section": "api",
+      "id": "twigs.globalHotkeys.service:GlobalHotkeysService",
+      "shortName": "GlobalHotkeysService",
+      "type": "service",
+      "moduleName": "twigs.globalHotkeys",
+      "shortDescription": "GlobalHotkeys allows you to assign actions to specific keyboard key combinations.",
+      "keywords": "$location actionfunction actions active add allows alt angular api app application array arrows assign buttons call callback combinations controller controllers current define defined directive events explicitly fields function global globalhotkeys globalhotkeysservice globally hotkey hotkeys html-element input invoked javascript key keyboard keycode keycodes keydown links main module order overridden override path path-specific pressed register registerglobalhotkey registerglobalhotkeycode registerglobalhotkeycodes registerglobalhotkeys registerpagehotkey registerpagehotkeycode registerpagehotkeycodes registerpagehotkeys route- service shift single somecontroller special specific string top-element trigger twg-global-hotkeys twigs var view work"
+    },
+    {
+      "section": "api",
+      "id": "twigs.globalPopups.provider:GlobalPopups",
+      "shortName": "GlobalPopups",
+      "type": "object",
+      "moduleName": "twigs.globalPopups",
+      "shortDescription": "GlobalPopups globally defines popups dialogs which can be called in every controller of your Angular JS application. They consist of two different types of dialogs:",
+      "keywords": "actions admin angular api application awesome backbuttontext boolean bootstrap button callback called cancel clicked clicks close consider consist content controller creation define defines description detailed dialogs dismiss dispaly displayed downloading error errordialog example false filedialog find footer frequently function github global globally globalpopups gui html http infodialog inform interpreted io javascript js left lightweight message method methods modal modals module nobuttontext object okbuttontext opened passing pdf popups popuptitle preconfigured predefined promise properties provider realy reason resolved resolving result returns string successmessage successtoast template templates text title toast toasts top true twigs types url user userctrl variables warningdialog warningmessage warnings warningtoast wrapper yesbuttontext yesnodialog"
+    },
+    {
+      "section": "api",
+      "id": "twigs.globalPopups.provider:GlobalPopupsProvider",
+      "shortName": "GlobalPopupsProvider",
+      "type": "object",
+      "moduleName": "twigs.globalPopups",
+      "shortDescription": "GlobalPopupsProvider can be used to define custom GlobalPopups or override twitch default GlobalPopups.",
+      "keywords": "$close additionally admin angular api automatically awesome backdrop block bootstrap browser browsers btn btn-default button class conatining config content controller create createfilemodal createmodal createtoast custom default define defines directly disappear display displayduration empty example false file filedialog files function github globalpopup globalpopups globalpopupsprovider hidden html http infodialog io javascript keyboard left location message messagename miliseconds modal modal-body modal-file modal-footer modal-header modal-info modal-myownpopup modaloptions modals module myapp myownpopup ng-click object options override pdf popup properties property provider required saved splayduration static template templates templateurl timeout title toast true twigs twitch type usage user userctrl views warningtoast windowclass"
+    },
+    {
+      "section": "api",
+      "id": "twigs.menu.directive:twgMenu",
+      "shortName": "twgMenu",
+      "type": "directive",
+      "moduleName": "twigs.menu",
+      "shortDescription": "In many web applications you will need navigations or menus which are present on all or multiple html pages. TwgMenu allows you",
+      "keywords": "access active allows alternatively api app applications change collapsable configured correspondent createmenu define directive directly ensures entry filter filtering globally html include included items links main_menu marked menu menu-name menues menuprovider menus multiple navigation navigations order parent properly protectedroutes provider referencing registers rendered rendering renders restricted roles route routes set template template-url twg twgmenu twigs twigs-menu users web"
+    },
+    {
+      "section": "api",
+      "id": "twigs.menu.provider:MenuProvider",
+      "shortName": "MenuProvider",
+      "type": "object",
+      "moduleName": "twigs.menu",
+      "shortDescription": "MenuProvider can be used to define menus globally which can later be used in multiple views.",
+      "keywords": "access accessible active activeroute additem adds angular api application attribute block child children class clicked complex config configuration configured content context corresponding create createmenu creates createsubmenu current currently define defined defines depend described determined direct directive directly display displayed enforced example exist exists fa fa-desktop fa-lg fa-lock filtered find function functionality getmenuiteminmenu globally href html iconclass included instance item itemname itemoptions items iterate javascript key length link list location main_menu main_menu_home main_menu_orders main_menu_orders_new_order main_menu_orders_overview main_menu_settings main_menu_settings_roles main_menu_settings_users mainmenu mapped mark marks matches meet menu menu-hover menu-icon menu-name menuitem menuitems menuname menuoptions menuprovider menus methods module multiple myapp navigating needed nested ng-class object openable opened options order orders ordersmenu overview parent path predefined property protected protectedrouteprovider provider referenced regex removed removemenu removes render restriction result returns role roles root route search searches settings-menu settingsmenu siblings strictly structure submenu submenu-label submenuitem submenus template templateurl text translation true twgmenu twigs undefined unique usage users var views window x-ng-class x-ng-href x-ng-if x-ng-repeat"
+    },
+    {
+      "section": "api",
+      "id": "twigs.menu.service:MenuPermissionService",
+      "shortName": "MenuPermissionService",
+      "type": "object",
+      "moduleName": "twigs.menu",
+      "keywords": "api filter filtered filtermenuforrouterestrictions filters function items menu object protected resolves routes security service twigs"
+    },
+    {
+      "section": "api",
+      "id": "twigs.protectedRoutes.provider:ProtectedRouteProvider",
+      "shortName": "ProtectedRouteProvider",
+      "type": "object",
+      "moduleName": "twigs.protectedRoutes",
+      "shortDescription": "In an application that uses a permission model, we&#39;d like to protect some views from",
+      "keywords": "$routeprovider _authenticated_ _protection_ access admin allows angular api app application appropriate changes check config configure controller depends display don evaluate event forward function handled homectrl html javascript loader location logged main message model module note object permission permissions prevented property protect protected protectedroute protectedrouteprovider protectedroutes protection provider registered required route route-change routechangestart routes security set settingsctrl specific standard templateurl thrown true twigs unauthorized user var view views"
+    },
+    {
+      "section": "api",
+      "id": "twigs.security.directive:twgProtected",
+      "shortName": "twgProtected",
+      "type": "directive",
+      "moduleName": "twigs.security",
+      "shortDescription": "Protects a html element: either hides it or disables it if currently logged in user does not have",
+      "keywords": "api attributes currently directive disable disables element equivalent evaluated evaluation evaluator hidden hide hides html logged missing permission permissions protects registered security set true twg-protected twg-protected-disable twg-protected-hide twigs user"
+    },
+    {
+      "section": "api",
+      "id": "twigs.security.provider:AuthorizerProvider",
+      "shortName": "AuthorizerProvider",
+      "type": "object",
+      "moduleName": "twigs.security",
+      "shortDescription": "",
+      "keywords": "$q $resource additional api args authorizer authorizerprovider call decide defer deferred evaluating evaluation evaluator expected false fn foo form function getuser ist javascript john load loader loading needed object pass permission permissionevaluator permissions promise properties property provider receive registerpermissionevaluationfunction registerpermissionevaluator registers registeruserloader registeruserloaderfunction reject remember resolve resolves return second security server-response somecollaborator true twigs user userloader userloaderfunction userloadingpromise username users valid var waiting"
+    },
+    {
+      "section": "api",
+      "id": "twigs.security.service:Authorizer",
+      "shortName": "Authorizer",
+      "type": "object",
+      "moduleName": "twigs.security",
+      "keywords": "$rootscope api backend broadcast call check checks clears clearsecuritycontext current currently directives evaluator event false function getuser haspermission holding invocation isloggedin load loaded logged-in needed object ont passed pending permission permissions promise registered rejected resolve resolved resolves returns security securitycontext service specifies trigger true twigs user usercleared userload userloaded wait"
+    },
+    {
+      "section": "api",
+      "id": "twigs.sortable.directive:twgSortable",
+      "shortName": "twgSortable",
+      "type": "directive",
+      "moduleName": "twigs.sortable",
+      "shortDescription": "In a modern user interface, we expect tables to be sortable.",
+      "keywords": "add addition allows api arrows ascending attribute classes click column controller css data define descending directive dummydata easy enables ensures example expect header headers html interface marker modern multiple ng-repeat ngrepeat number orderby rows scope second separate set sort sortable sorted specific styling table tables tag td twg-sortable twg-sortabletable twgsortable twigs user usual variables view"
+    },
+    {
+      "section": "api",
+      "id": "twigs.tableRowClick.directive:twgTableRowClick",
+      "shortName": "twgTableRowClick",
+      "type": "directive",
+      "moduleName": "twigs.tableRowClick",
+      "shortDescription": "For a better user experience, we want to be able to react to mouseclicks anywhere on a table row, not just one link in a cell.",
+      "keywords": "additionally adds angular api app better bubble button cell change click clicks correctly directive dosomething elements events example experience handle handlers href html ignoring invoke javascript link links listener location main method module mouse mouseclicks ng-click react routing row rows second switches table tablerowclick text third tr trigger twg-table-row-click twigs url user users var x-ng-repeat"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api",
+    "scripts": [
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js",
+      "js/prettify.js"
+    ],
+    "styles": [],
+    "title": "Twigs Documentation",
+    "html5Mode": false,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "animation": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js",
+    "angular-animate.min.js",
+    "marked.js",
+    "prettify.js"
+  ]
+};
